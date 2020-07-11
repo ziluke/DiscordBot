39e852822d6155438026ffacc78e632696c54dd7
Lukas <lukasvictor99@yahoo.com>
2020-07-11 20:39:52 +0300

    Final result

---
+++ commands/aaaa.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'aaaa',
+    description: 'AAAaaa!',
+    execute(msg, args) {
+      msg.reply('https://www.youtube.com/watch?v=du-TY1GUFGk&feature=youtu.be&t=2');
+      //msg.channel.send('Aaaaaaaaaaa');
+    },
+  };
